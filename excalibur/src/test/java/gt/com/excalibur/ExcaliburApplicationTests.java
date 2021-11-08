db73efbca85ffea9e7a5a2229be32c181e25d6d3
Marvin <malopez@takitosu.localdomain>
2021-11-07 22:39:07 -0600

    Agregando proyecto base

---
+++ excalibur/src/test/java/gt/com/excalibur/ExcaliburApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.com.excalibur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcaliburApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
